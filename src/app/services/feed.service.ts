27c990b079e897023cddc73c8974a3f4fcd15de8
Ayelet Dahan <anativ@gmail.com>
2019-06-27 15:35:17 +0300

    removed old comment

--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -15,19 +15,4 @@ export class FeedService {
     const path = 'assets/' + filename;
     return this.http.get(path);
   }
-
-  /**
-   * In-place sort of a FeedArray
-   * TODO: create the feed as a 
-   * @param feed A feed to sort
-   * @param by key to sort by. Default is title
-   * @param order sort order. default ASC. any other value will be considered DSC (TODO)
-   */
-  // sort(feed:FeedArray, by:string="title", order:string="ASC"):FeedArray {
-  //   return feed.sort((a,b) => {
-  //     if (a[by] < b[by]) return order === "ASC" ?-1:1;
-  //     if (a[by] > b[by]) return order === "ASC" ?1:-1;
-  //     return 0;
-  //   });
-  // }
 }
